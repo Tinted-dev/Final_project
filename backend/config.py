1dee1e54f6aab50d2659e8c47f484c50efa2e3c7
Dennis <mumbidenis@gmail.com>
2025-05-22 09:51:30 +0300

    Prepare for Render deployment

--- backend/config.py
+++ backend/config.py
@@ -4,10 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
+
+    # --- CHANGE THIS LINE ---
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
-        'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
+        'postgresql://garbage_app_db_user:4v9V892DME2PdFIy7xPvYVXxtOzjFcfi@dpg-d0nc4uumcj7s73dr2vr0-a/garbage_app_db'
     )
+    # --- END CHANGE ---
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret-key')
